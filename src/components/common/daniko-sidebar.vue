7dce2c5ea182c4ab57f8529786b85926f9311970
Azatyk <azatuk2005@mail.ru>
2021-04-26 17:34:02 +0600

    feat: add sidebar and sidebar layout

---
+++ src/components/common/daniko-sidebar.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-main">
+      <div class="sidebar-logo">
+        <img
+          src="@/assets/images/logo.svg"
+          alt="Daniko"
+          class="sidebar-logo-image"
+        />
+      </div>
+      <div class="sidebar-links">
+        <router-link class="sidebar-link" to="/workers">
+          <i class="bx bxs-user-account sidebar-link-icon"></i>
+          <span class="sidebar-link-label">Специалисты</span>
+        </router-link>
+        <router-link class="sidebar-link" to="/services">
+          <i class="bx bxs-select-multiple sidebar-link-icon"></i>
+          <span class="sidebar-link-label">Услуги</span>
+        </router-link>
+        <router-link class="sidebar-link" to="/contacts">
+          <i class="bx bxs-phone sidebar-link-icon"></i>
+          <span class="sidebar-link-label">Контакты</span>
+        </router-link>
+      </div>
+    </div>
+    <div class="sidebar-links">
+      <div class="sidebar-link">
+        <i class="bx bxs-user-circle sidebar-link-icon"></i>
+        <span class="sidebar-link-label">Профиль</span>
+      </div>
+      <div class="sidebar-link sidebar-link-danger">
+        <i class="bx bx-log-out sidebar-link-icon"></i>
+        <span class="sidebar-link-label">Выход</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/variables.scss";
+
+.sidebar {
+  padding: 0 20px 40px 40px;
+  width: 300px;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: $white;
+
+  &-main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-logo {
+    margin-bottom: 40px;
+    width: 90%;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    border-bottom: 2px solid #b2bec330;
+
+    &-image {
+      width: 75%;
+    }
+  }
+
+  &-links {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-link {
+    margin-bottom: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    color: $main-dark;
+    text-decoration: none;
+    cursor: pointer;
+    transition: 200ms ease-in-out;
+    opacity: 0.6;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &-icon {
+      margin-right: 15px;
+      font-size: 22px;
+    }
+
+    &-label {
+      font-size: 16px;
+    }
+
+    &-danger {
+      &:hover {
+        color: #e74c3c;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/layouts/daniko-sidebar-layout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    <daniko-sidebar class="page-sidebar" />
+    <div class="page-content"></div>
+  </div>
+</template>
+
+<script>
+import danikoSidebar from "@/components/common/daniko-sidebar.vue";
+
+export default {
+  components: {
+    "daniko-sidebar": danikoSidebar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/variables.scss";
+
+.page {
+  position: relative;
+
+  &-sidebar {
+    position: fixed;
+    left: 0;
+    top: 0;
+  }
+
+  &-content {
+    margin-left: 300px;
+    width: calc(100vw - 300px);
+    min-height: 100vh;
+  }
+}
+</style>
