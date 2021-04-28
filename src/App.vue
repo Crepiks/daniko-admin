614a190acb635b673202b2753bf7cdf1fd00e5cc
Azatyk <azatuk2005@mail.ru>
2021-04-28 10:21:58 +0600

    feat: add rcontacts edit block

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    if (this.$route.path == "/") {
+      this.$router.push("/workers");
+    }
+  },
+};
+</script>
+
 <style lang="scss">
 @import "@/assets/styles/global.scss";
 </style>
--- src/views/daniko-contacts.vue
+++ src/views/daniko-contacts.vue
@@ -1,8 +1,55 @@
 <template>
   <div class="contacts-page">
+    <daniko-right-block
+      :isOpen="isRightBlockOpen"
+      title="Редактирование контаков"
+      @close-right-block="isRightBlockOpen = false"
+    >
+      <div class="right-block-component">
+        <daniko-input
+          class="right-block-input"
+          title="Номер телефона"
+          type="tel"
+          placeholder="Введите контактный номер телефона"
+          v-model="newContacts.phoneNumber"
+        />
+        <daniko-input
+          class="right-block-input"
+          title="Электрооная почта"
+          type="email"
+          placeholder="Введите контактную электронную почту"
+          v-model="newContacts.email"
+        />
+        <daniko-input
+          class="right-block-input"
+          title="Почтовый индекс"
+          placeholder="Введите почтовый индекс"
+          v-model="newContacts.mailIndex"
+        />
+        <daniko-input
+          class="right-block-input"
+          title="Координаты"
+          type="text"
+          placeholder="42.924320, 71.380932"
+          v-model="newContacts.coords"
+        />
+        <div class="right-block-map">
+          <yandex-map
+            :coords="getNewCoords"
+            :zoom="18"
+            style="width: 100%; height: 100%"
+          >
+            <ymap-marker marker-id="1" :coords="getNewCoords"></ymap-marker>
+          </yandex-map>
+        </div>
+        <daniko-button class="right-block-button">Сохранить</daniko-button>
+      </div>
+    </daniko-right-block>
     <header class="contacts-header">
       <h2 class="contacts-title">Контакты</h2>
-      <daniko-button>Редактировать</daniko-button>
+      <daniko-button @click="isRightBlockOpen = true"
+        >Редактировать</daniko-button
+      >
     </header>
     <div class="contacts-content">
       <div class="contacts-container">
@@ -43,17 +90,56 @@
 
 <script>
 import danikoButton from "@/components/common/daniko-button.vue";
+import danikoInput from "@/components/common/daniko-input.vue";
+import danikoRightBlock from "@/components/common/daniko-right-block.vue";
 import contacts from "@/data/contacts.js";
 
 export default {
   components: {
     "daniko-button": danikoButton,
+    "daniko-right-block": danikoRightBlock,
+    "daniko-input": danikoInput,
   },
 
   data() {
     return {
       contacts: contacts,
+      isRightBlockOpen: false,
+      newContacts: {
+        phoneNumber: "",
+        email: "",
+        mailIndex: "",
+        mapcoords: "",
+        coords: "",
+      },
     };
+  },
+
+  watch: {
+    isRightBlockOpen() {
+      if (this.isRightBlockOpen) {
+        this.newContacts.phoneNumber = this.contacts.phoneNumber;
+        this.newContacts.email = this.contacts.email;
+        this.newContacts.mailIndex = this.contacts.mailIndex;
+        this.newContacts.coords =
+          String(this.contacts.coords[0]) +
+          ", " +
+          String(this.contacts.coords[1]);
+      }
+    },
+  },
+
+  computed: {
+    getNewCoords() {
+      if (this.newContacts.coords.trim()) {
+        const coords = this.newContacts.coords.split(",");
+        coords[0] = Number(coords[0]);
+        coords[1] = Number(coords[1]);
+        return coords;
+      } else {
+        return [0, 0];
+      }
+    },
   },
 };
 </script>
@@ -124,6 +210,32 @@ export default {
 
   &-data {
     color: $primary;
+  }
+}
+
+.right-block {
+  &-component {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-input {
+    margin-bottom: 25px;
+  }
+
+  &-button {
+    margin-top: 10px;
+  }
+
+  &-map {
+    margin-bottom: 25px;
+    width: 100%;
+    height: 200px;
+    border-radius: 10px;
+    background-color: #dbdbdb;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.05);
+    overflow: hidden;
   }
 }
 </style>
